2f58d6f15883da3168b0cf15ac8ee3bb58f73f1f
Robin Doer <robin@robind.de>
2011-11-27 18:21:38 +0100

    SWTServerHandler.exceptionCaught()

--- de.robind.swt.server/src/de/robind/swt/server/SWTServerHandler.java
+++ de.robind.swt.server/src/de/robind/swt/server/SWTServerHandler.java
@@ -1,5 +1,8 @@
 package de.robind.swt.server;
 
+import org.apache.log4j.Logger;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 
 /**
@@ -8,5 +11,17 @@ import org.jboss.netty.channel.SimpleChannelHandler;
  * @author Robin Doer
  */
 public class SWTServerHandler extends SimpleChannelHandler {
+  /**
+   * Logger of the class.
+   */
+  private static final Logger logger = Logger.getLogger(SWTServerHandler.class);
 
+  /* (non-Javadoc)
+   * @see org.jboss.netty.channel.SimpleChannelHandler#exceptionCaught(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ExceptionEvent)
+   */
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+    logger.error("Exception caught, closing channel", e.getCause());
+    e.getChannel().close();
+  }
 }
