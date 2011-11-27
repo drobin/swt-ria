dc271841dd272138511529a1454cbc5964573738
Robin Doer <robin@robind.de>
2011-11-27 19:08:52 +0100

    SWTClientHandler.exceptionCaught

--- de.robind.swt.client/src/de/robind/swt/client/SWTClientHandler.java
+++ de.robind.swt.client/src/de/robind/swt/client/SWTClientHandler.java
@@ -1,6 +1,10 @@
 package de.robind.swt.client;
 
+import org.apache.log4j.Logger;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
+
 
 /**
  * This channel-handler handles incoming and outgoing messages for the client.
@@ -8,6 +12,11 @@ import org.jboss.netty.channel.SimpleChannelHandler;
  * @author Robin Doer
  */
 public class SWTClientHandler extends SimpleChannelHandler {
+  /**
+   * Logger of the class.
+   */
+  private static final Logger logger = Logger.getLogger(SWTClientHandler.class);
+
   /**
    * Environment of the client.
    */
@@ -20,5 +29,14 @@ public class SWTClientHandler extends SimpleChannelHandler {
    */
   public SWTClientHandler(SWTClientEnvironment env) {
     this.env = env;
+  }
+
+  /* (non-Javadoc)
+   * @see org.jboss.netty.channel.SimpleChannelHandler#exceptionCaught(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ExceptionEvent)
+   */
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+    logger.error("exceptionCaught, closing channel", e.getCause());
+    ctx.getChannel().close();
   }
 }
