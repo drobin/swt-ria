6cf77b5374d381143aa83381288b191803b828d6
Robin Doer <robin@robind.de>
2011-11-27 19:35:27 +0100

    SWTClient connects to the server

--- de.robind.swt.client/src/de/robind/swt/client/SWTClient.java
+++ de.robind.swt.client/src/de/robind/swt/client/SWTClient.java
@@ -1,21 +1,47 @@
 package de.robind.swt.client;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 
 public class SWTClient {
   public static void main(String[] args) {
     Display display = new Display();
-    Shell shell = new Shell(display);
 
+    Shell shell = new Shell(display);
     shell.open();
 
-    while (!shell.isDisposed()) {
+    SWTClientEnvironment env = new SWTClientEnvironment(display);
+
+    ChannelFactory factory = new NioClientSocketChannelFactory(
+        Executors.newCachedThreadPool(),
+        Executors.newCachedThreadPool());
+
+    ClientBootstrap bootstrap = new ClientBootstrap(factory);
+    bootstrap.setPipelineFactory(new PipelineFactory(env));
+    bootstrap.setOption("tcpNoDelay", true);
+    bootstrap.setOption("keepAlive", true);
+
+    ChannelFuture future =
+        bootstrap.connect(new InetSocketAddress("localhost", 4711));
+    future.awaitUninterruptibly();
+
+    while (future.getChannel().isConnected() && !shell.isDisposed()) {
       if (!display.readAndDispatch()) {
         display.sleep();
       }
     }
 
     display.dispose();
+    future.getChannel().close();
+
+    future.getChannel().getCloseFuture().awaitUninterruptibly();
+    factory.releaseExternalResources();
   }
 }
