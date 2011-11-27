8b9d6ea692dfa34d93ce8691b326fb60f38b4c36
Robin Doer <robin@robind.de>
2011-11-27 18:30:59 +0100

    SWTClient-skeleton

---
+++ de.robind.swt.client/src/de/robind/swt/client/SWTClient.java
@@ -0,0 +1,21 @@
+package de.robind.swt.client;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class SWTClient {
+  public static void main(String[] args) {
+    Display display = new Display();
+    Shell shell = new Shell(display);
+
+    shell.open();
+
+    while (!shell.isDisposed()) {
+      if (!display.readAndDispatch()) {
+        display.sleep();
+      }
+    }
+
+    display.dispose();
+  }
+}
