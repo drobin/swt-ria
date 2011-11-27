901af617abde0a36ca28068c89b0536b6088c2b8
Robin Doer <robin@robind.de>
2011-11-27 13:55:09 +0100

    SWTServer-skeleton

---
+++ de.robind.swt.server/src/de/robind/swt/server/SWTServer.java
@@ -0,0 +1,30 @@
+package de.robind.swt.server;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+public class SWTServer {
+  public static void main(String[] args) {
+    ChannelFactory factory = new NioServerSocketChannelFactory(
+        Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+    ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+    bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+      public ChannelPipeline getPipeline() throws Exception {
+        return (Channels.pipeline());
+      }
+    });
+
+    bootstrap.setOption("child.tcpNoDelay", true);
+    bootstrap.setOption("child.keepAlive", true);
+
+    bootstrap.bind(new InetSocketAddress(4711));
+  }
+}
