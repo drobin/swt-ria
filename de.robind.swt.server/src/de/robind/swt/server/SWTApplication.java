213ef6373a093342685509bb6df54a9933827b0e
Robin Doer <robin@robind.de>
2011-11-28 13:39:41 +0100

    SWTApplication-skeleton

---
+++ de.robind.swt.server/src/de/robind/swt/server/SWTApplication.java
@@ -0,0 +1,13 @@
+package de.robind.swt.server;
+
+/**
+ * An application running on the application-server
+ *
+ * @author Robin Doer
+ */
+public class SWTApplication extends Thread {
+  @Override
+  public void run() {
+
+  }
+}
--- de.robind.swt.server/src/de/robind/swt/server/SWTServerHandler.java
+++ de.robind.swt.server/src/de/robind/swt/server/SWTServerHandler.java
@@ -2,6 +2,7 @@ package de.robind.swt.server;
 
 import org.apache.log4j.Logger;
 import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 
@@ -15,6 +16,34 @@ public class SWTServerHandler extends SimpleChannelHandler {
    * Logger of the class.
    */
   private static final Logger logger = Logger.getLogger(SWTServerHandler.class);
+
+  /* (non-Javadoc)
+   * @see org.jboss.netty.channel.SimpleChannelHandler#channelConnected(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ChannelStateEvent)
+   */
+  @Override
+  public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e)
+      throws Exception {
+
+    logger.debug("Connected: " + ctx.getChannel());
+
+    SWTApplication application = new SWTApplication();
+    application.start();
+
+    ctx.setAttachment(application);
+  }
+
+  /* (non-Javadoc)
+   * @see org.jboss.netty.channel.SimpleChannelHandler#channelClosed(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ChannelStateEvent)
+   */
+  @Override
+  public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e)
+      throws Exception {
+
+    logger.debug("Closed: " + ctx.getChannel());
+
+    SWTApplication application = (SWTApplication)ctx.getAttachment();
+    application.join();
+  }
 
   /* (non-Javadoc)
    * @see org.jboss.netty.channel.SimpleChannelHandler#exceptionCaught(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ExceptionEvent)
