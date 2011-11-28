d16146347d34be9f6cfcdb84e9677f43aac22c57
Robin Doer <robin@robind.de>
2011-11-28 14:41:40 +0100

    ClientTasksImpl sends the request and waits for the response

--- de.robind.swt.server/src/de/robind/swt/server/ClientTasksImpl.java
+++ de.robind.swt.server/src/de/robind/swt/server/ClientTasksImpl.java
@@ -2,6 +2,7 @@ package de.robind.swt.server;
 
 import org.eclipse.swt.server.ClientTasks;
 import org.eclipse.swt.server.Key;
+import org.jboss.netty.channel.Channels;
 
 import de.robind.swt.msg.SWTRequest;
 import de.robind.swt.msg.SWTResponse;
@@ -17,6 +18,16 @@ public class ClientTasksImpl implements ClientTasks {
    * @see org.eclipse.swt.server.ClientTasks#sendRequest(org.eclipse.swt.server.Key, de.robind.swt.msg.SWTRequest)
    */
   public SWTResponse sendRequest(Key key, SWTRequest request) {
-    throw new Error("Needs to be implemented");
+    SWTApplicationKey appKey = (SWTApplicationKey)key;
+    SWTApplication app = appKey.getApplication();
+
+    Channels.write(app.getChannel(), request);
+
+    try {
+      return (app.getResponseQueue().take());
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      return (null);
+    }
   }
 }
