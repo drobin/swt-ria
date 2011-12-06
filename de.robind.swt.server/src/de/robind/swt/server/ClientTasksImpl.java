6938ccc4435c7f10211eef42255c034e87db3154
Robin Doer <robin@robind.de>
2011-12-06 22:25:01 +0100

    Evaluate exception before regular result

--- de.robind.swt.server/src/de/robind/swt/server/ClientTasksImpl.java
+++ de.robind.swt.server/src/de/robind/swt/server/ClientTasksImpl.java
@@ -33,13 +33,13 @@ public class ClientTasksImpl implements ClientTasks {
     Channels.write(app.getChannel(), request);
     SWTResponse response = app.getResponseQueue().take();
 
+    if (response instanceof SWTException) {
+      throw ((SWTException)response).getCause();
+    }
+
     if (!(response instanceof SWTNewResponse)) {
       throw new Exception("Illegal response of type " +
           response.getClass().getName() + " received");
-    }
-
-    if (response instanceof SWTException) {
-      throw ((SWTException)response).getCause();
     }
   }
 
