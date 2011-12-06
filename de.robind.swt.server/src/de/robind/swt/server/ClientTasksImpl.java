bd83051b51cdb0b2a4aa42b1e6f855bd5340077f
Robin Doer <robin@robind.de>
2011-12-06 22:04:43 +0100

    ClientTasksImpl.normalizeArguments()

--- de.robind.swt.server/src/de/robind/swt/server/ClientTasksImpl.java
+++ de.robind.swt.server/src/de/robind/swt/server/ClientTasksImpl.java
@@ -1,5 +1,6 @@
 package de.robind.swt.server;
 
+import org.eclipse.swt.SWTObject;
 import org.eclipse.swt.server.ClientTasks;
 import org.eclipse.swt.server.Key;
 import org.jboss.netty.channel.Channels;
@@ -9,6 +10,7 @@ import de.robind.swt.msg.SWTCallResponse;
 import de.robind.swt.msg.SWTException;
 import de.robind.swt.msg.SWTNewRequest;
 import de.robind.swt.msg.SWTNewResponse;
+import de.robind.swt.msg.SWTObjectId;
 import de.robind.swt.msg.SWTResponse;
 
 /**
@@ -25,8 +27,8 @@ public class ClientTasksImpl implements ClientTasks {
 
     SWTApplicationKey appKey = (SWTApplicationKey)key;
     SWTApplication app = appKey.getApplication();
-    SWTNewRequest request =
-        app.getMessageFactory().createNewRequest(id, objClass, args);
+    SWTNewRequest request = app.getMessageFactory().createNewRequest(
+        id, objClass, normalizeArguments(args));
 
     Channels.write(app.getChannel(), request);
     SWTResponse response = app.getResponseQueue().take();
@@ -49,8 +51,8 @@ public class ClientTasksImpl implements ClientTasks {
 
     SWTApplicationKey appKey = (SWTApplicationKey)key;
     SWTApplication app = appKey.getApplication();
-    SWTCallRequest request =
-        app.getMessageFactory().createCallRequest(id, method, args);
+    SWTCallRequest request = app.getMessageFactory().createCallRequest(
+        id, method, normalizeArguments(args));
 
     Channels.write(app.getChannel(), request);
     SWTResponse response = app.getResponseQueue().take();
@@ -74,5 +76,28 @@ public class ClientTasksImpl implements ClientTasks {
       throws Throwable {
 
     throw new Error("Needs to be implemented");
+  }
+
+  /**
+   * Normalize the given argument-list.
+   * <p>
+   * If a {@link SWTObject} is placed into the array, it is replaced by the
+   * related {@link SWTObjectId}.
+   *
+   * @param args Arguments which should be normalized
+   * @return Normalized arguments
+   */
+  private Object[] normalizeArguments(Object args[]) {
+    Object result[] = new Object[args.length];
+
+    for (int i = 0; i < args.length; i++) {
+      if (args[i] instanceof SWTObject) {
+        result[i] = new SWTObjectId(((SWTObject)args[i]).getId());
+      } else {
+        result[i] = args[i];
+      }
+    }
+
+    return (result);
   }
 }
