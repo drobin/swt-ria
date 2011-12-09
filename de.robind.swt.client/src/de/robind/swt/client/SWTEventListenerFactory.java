fbb5ab1de81cb4f0b3982c4dab8b29074c1f725e
Robin Doer <robin@robind.de>
2011-12-09 18:11:10 +0100

    SWTEventListenerFactory

---
+++ de.robind.swt.client/src/de/robind/swt/client/SWTEventListenerFactory.java
@@ -0,0 +1,37 @@
+package de.robind.swt.client;
+
+import org.jboss.netty.channel.Channel;
+
+import de.robind.swt.msg.SWTMessageFactory;
+
+/**
+ * Factory used to create {@link SWTEventListener}-instances.
+ *
+ * @author Robin Doer
+ */
+public class SWTEventListenerFactory {
+  /**
+   * The channel to the server
+   */
+  private Channel channel = null;
+
+  /**
+   * Factory used to create messages.
+   */
+  private SWTMessageFactory messageFactory = null;
+
+  public SWTEventListenerFactory(Channel channel, SWTMessageFactory messageFactory) {
+    this.channel = channel;
+    this.messageFactory = messageFactory;
+  }
+
+  /**
+   * Creates a new {@link SWTEventListener}-instance.
+   *
+   * @param objId The object-id of the widget maintained by the listener
+   * @return The new listener
+   */
+  public SWTEventListener createListener(int objId) {
+    return (new SWTEventListener(objId, channel, messageFactory));
+  }
+}
