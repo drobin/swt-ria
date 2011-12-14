67d3af7b73979aca5b64da27b65b7ed9b59fa17f
Robin Doer <robin@robind.de>
2011-12-14 18:38:31 +0100

    Insert list with addressbook.entries into AddressBook-demo

--- de.robind.swt.demo/src/de/robind/swt/demo/addressbook/AddressBookDemo.java
+++ de.robind.swt.demo/src/de/robind/swt/demo/addressbook/AddressBookDemo.java
@@ -1,5 +1,8 @@
 package de.robind.swt.demo.addressbook;
 
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -9,6 +12,12 @@ public class AddressBookDemo {
 
     Shell shell = new Shell(display);
     shell.setText("Addressbook");
+    shell.setLayout(new FillLayout());
+
+    ListViewer listViewer = new ListViewer(shell, SWT.SINGLE);
+    listViewer.setLabelProvider(new AddressBookLabelProvider());
+    listViewer.setContentProvider(new AddressBookContentProvider());
+    listViewer.setInput(new AddressBookModel());
 
     shell.open();
     while (!shell.isDisposed()) {
