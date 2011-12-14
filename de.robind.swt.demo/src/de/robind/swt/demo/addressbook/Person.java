dc2d01dc529261dd0ee462109047d61012f09ffe
Robin Doer <robin@robind.de>
2011-12-14 18:11:20 +0100

    Person-POJO

---
+++ de.robind.swt.demo/src/de/robind/swt/demo/addressbook/Person.java
@@ -0,0 +1,22 @@
+package de.robind.swt.demo.addressbook;
+
+public class Person {
+  private String firstName = null;
+  private String lastName = null;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
