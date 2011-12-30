bd884f0fe43ef55a8dd45659ed72aea4798b8832
Robin Doer <robin@robind.de>
2011-12-30 12:49:09 +0100

    Test ControlListener

---
+++ de.robind.swt/test/org/eclipse/swt/test/TestControlListener.java
@@ -0,0 +1,25 @@
+package org.eclipse.swt.test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
+
+public class TestControlListener implements ControlListener {
+  public List<ControlEvent> movedEvents = new LinkedList<ControlEvent>();
+  public List<ControlEvent> resizedEvents = new LinkedList<ControlEvent>();
+
+  public void controlMoved(ControlEvent e) {
+    this.movedEvents.add(e);
+  }
+
+  public void controlResized(ControlEvent e) {
+    this.resizedEvents.add(e);
+  }
+
+  public void clearEvents() {
+    this.movedEvents.clear();
+    this.resizedEvents.clear();
+  }
+}
--- de.robind.swt/test/org/eclipse/swt/widget/test/ControlTest.java
+++ de.robind.swt/test/org/eclipse/swt/widget/test/ControlTest.java
@@ -2,6 +2,7 @@ package org.eclipse.swt.widget.test;
 
 import static org.eclipse.swt.test.SWTExceptionMatcher.swtCode;
 import static org.eclipse.swt.test.SWTTestUtils.asyncExec;
+import static org.eclipse.swt.test.TypedEventMatcher.event;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -12,6 +13,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.server.DisplayPool;
 import org.eclipse.swt.server.Key;
 import org.eclipse.swt.test.TestClientTasks;
+import org.eclipse.swt.test.TestControlListener;
+import org.eclipse.swt.test.TestEvent;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -88,5 +91,105 @@ public class ControlTest {
   public void ctorStyle() {
     Control control = new Control(this.shell, 4711) {};
     assertThat(control.getStyle(), is(4711));
+  }
+
+  @Test
+  public void addControlListenerNullListener() {
+    exception.expect(swtCode(SWT.ERROR_NULL_ARGUMENT));
+
+    Control control = new Control(this.shell, 0) {};
+    control.addControlListener(null);
+  }
+
+  @Test
+  public void addControlListenerDisposed() {
+    exception.expect(swtCode(SWT.ERROR_WIDGET_DISPOSED));
+
+    Control control = new Control(this.shell, 0) {};
+    control.dispose();
+    control.addControlListener(new TestControlListener());
+  }
+
+  @Test
+  public void addControlListenerInvalidThread() throws Throwable {
+    exception.expect(swtCode(SWT.ERROR_THREAD_INVALID_ACCESS));
+
+    final Control control = new Control(this.shell, 0) {};
+    asyncExec(new Callable<Control>() {
+      public Control call() throws Exception {
+        control.addControlListener(new TestControlListener());
+        return (control);
+      }
+    });
+  }
+
+  @Test
+  public void removeControlListenerNullListener() {
+    exception.expect(swtCode(SWT.ERROR_NULL_ARGUMENT));
+
+    Control control = new Control(this.shell, 0) {};
+    control.addControlListener(null);
+  }
+
+  @Test
+  public void removeControlListenerDisposed() {
+    exception.expect(swtCode(SWT.ERROR_WIDGET_DISPOSED));
+
+    Control control = new Control(this.shell, 0) {};
+    control.dispose();
+    control.addControlListener(new TestControlListener());
+  }
+
+  @Test
+  public void removeControlListenerInvalidThread() throws Throwable {
+    exception.expect(swtCode(SWT.ERROR_THREAD_INVALID_ACCESS));
+
+    final Control control = new Control(this.shell, 0) {};
+    asyncExec(new Callable<Control>() {
+      public Control call() throws Exception {
+        control.addControlListener(new TestControlListener());
+        return (control);
+      }
+    });
+  }
+
+  @Test
+  public void controlListenerHandling() {
+    Control control = new Control(this.shell, 0) {};
+    TestControlListener listener = new TestControlListener();
+    TestEvent event1 = new TestEvent(1);
+    TestEvent event2 = new TestEvent(2);
+    TestEvent event3 = new TestEvent(3);
+    TestEvent event4 = new TestEvent(4);
+
+    assertThat(control.getListeners(SWT.Move).length, is(0));
+    assertThat(control.getListeners(SWT.Resize).length, is(0));
+    control.addControlListener(listener);
+    assertThat(control.getListeners(SWT.Move).length, is(1));
+    assertThat(control.getListeners(SWT.Resize).length, is(1));
+
+    control.notifyListeners(SWT.Move, event1);
+    control.notifyListeners(SWT.Resize, event2);
+    control.notifyListeners(SWT.Move, event3);
+    control.notifyListeners(SWT.Resize, event4);
+
+    assertThat(listener.movedEvents.size(), is(2));
+    assertThat(listener.movedEvents.get(0), is(event(display, control, 1)));
+    assertThat(listener.movedEvents.get(1), is(event(display, control, 3)));
+    assertThat(listener.resizedEvents.get(0), is(event(display, control, 2)));
+    assertThat(listener.resizedEvents.get(1), is(event(display, control, 4)));
+
+    listener.clearEvents();
+    control.removeControlListener(listener);
+    assertThat(control.getListeners(SWT.Move).length, is(0));
+    assertThat(control.getListeners(SWT.Resize).length, is(0));
+
+    control.notifyListeners(SWT.Move, event1);
+    control.notifyListeners(SWT.Resize, event2);
+    control.notifyListeners(SWT.Move, event3);
+    control.notifyListeners(SWT.Resize, event4);
+
+    assertThat(listener.movedEvents.size(), is(0));
+    assertThat(listener.resizedEvents.size(), is(0));
   }
 }
