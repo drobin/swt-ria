a23fa1b573caac65fa0f2b9285ed7f36ee2659c6
Robin Doer <robin@robind.de>
2012-01-03 13:11:24 +0100

    Encode/decode an integer-array

--- de.robind.swt.protocol/src/de/robind/swt/protocol/datatype/SWTInteger.java
+++ de.robind.swt.protocol/src/de/robind/swt/protocol/datatype/SWTInteger.java
@@ -1,6 +1,7 @@
 package de.robind.swt.protocol.datatype;
 
 import static de.robind.swt.protocol.datatype.DataTypeUtils.DT_INT;
+import static de.robind.swt.protocol.datatype.DataTypeUtils.FLAG_ARRAY;
 import static de.robind.swt.protocol.datatype.DataTypeUtils.FLAG_NONE;
 import static de.robind.swt.protocol.datatype.DataTypeUtils.fetchAndCheckFlags;
 import static de.robind.swt.protocol.datatype.DataTypeUtils.fetchAndCheckType;
@@ -8,7 +9,6 @@ import static de.robind.swt.protocol.datatype.DataTypeUtils.fetchAndCheckType;
 import org.jboss.netty.buffer.ChannelBuffer;
 
 import de.robind.swt.protocol.SWTProtocolException;
-
 /**
  * Utilitiy-methods to encode/decode an {@link Integer} from a
  * {@link ChannelBuffer}.
@@ -41,11 +41,45 @@ public class SWTInteger {
   }
 
   /**
+   * Reads an integer-array from the buffer.
+   *
+   * @param buffer the source buffer
+   * @return the decoded integer-array
+   * @throws NullPointerException if <code>buffer</code> is <code>null</code>
+   * @throws IndexOutOfBoundsException if not enough data are available in
+   *         <code>buffer</code> to read the integer-array
+   * @throws SWTProtocolException if an decoding-error occured
+   */
+  public static int[] readIntegerArray(ChannelBuffer buffer)
+      throws NullPointerException, IndexOutOfBoundsException,
+             SWTProtocolException {
+
+    if (buffer == null) {
+      throw new NullPointerException("buffer cannot be null");
+    }
+
+    fetchAndCheckType(buffer, DT_INT);
+    fetchAndCheckFlags(buffer, FLAG_ARRAY);
+
+    short length = buffer.readShort();
+    if (length < 0) {
+      throw new SWTProtocolException("Length of array cannot be negative");
+    }
+
+    int result[] = new int[length];
+    for (int i = 0; i < length; i++) {
+      result[i] = buffer.readInt();
+    }
+
+    return (result);
+  }
+
+  /**
    * Writes an integer back into the buffer.
    *
    * @param buffer The destination buffer
    * @param integer The integer to be encoded
-   * @throws NullPointerException of <code>buffer</code> is <code>null</code>
+   * @throws NullPointerException if <code>buffer</code> is <code>null</code>
    * @throws IndexOutOfBoundsException if not enough space is available in
    *         <code>buffer</code> to encode the whole <code>integer</code>
    */
@@ -59,5 +93,37 @@ public class SWTInteger {
     buffer.writeByte(DT_INT);
     buffer.writeByte(FLAG_NONE);
     buffer.writeInt(integer);
+  }
+
+  /**
+   * Writes an integer-array back into the buffer.
+   *
+   * @param buffer the destination buffer
+   * @param array the integer-array to be encoded
+   * @throws NullPointerException if <code>buffer</code> is <code>null</code>
+   * @throws IndexOutOfBoundsException if not enough space is available in
+   *         <code>buffer</code> to encode the whole array
+   * @throws SWTProtocolException if an encoding-error occured
+   */
+  public static void writeIntegerArray(ChannelBuffer buffer, int array[])
+      throws NullPointerException, IndexOutOfBoundsException,
+             SWTProtocolException {
+
+    if (buffer == null) {
+      throw new NullPointerException("buffer cannot be null");
+    }
+
+    if (array.length > Short.MAX_VALUE) {
+      throw new SWTProtocolException(
+          "Length of array cannot be greater than " + Short.MAX_VALUE);
+    }
+
+    buffer.writeByte(DT_INT);
+    buffer.writeByte(FLAG_ARRAY);
+    buffer.writeShort(array.length);
+
+    for (int value: array) {
+      buffer.writeInt(value);
+    }
   }
 }
--- de.robind.swt.protocol/test/de/robind/swt/protocol/datatypes/tests/SWTIntegerTest.java
+++ de.robind.swt.protocol/test/de/robind/swt/protocol/datatypes/tests/SWTIntegerTest.java
@@ -78,6 +78,75 @@ public class SWTIntegerTest {
   }
 
   @Test
+  public void readIntegerArrayNullBuffer() throws SWTProtocolException {
+    exception.expect(NullPointerException.class);
+    exception.expectMessage("buffer cannot be null");
+
+    SWTInteger.readIntegerArray(null);
+  }
+
+  @Test
+  public void readIntegerArrayUnexpectedType() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+    buffer.writeByte(0);
+
+    exception.expect(SWTProtocolException.class);
+    exception.expectMessage("Unexpected type read from buffer. Expected 2, but read 0");
+
+    SWTInteger.readIntegerArray(buffer);
+  }
+
+  @Test
+  public void readIntegerArrayUnexpectedFlags() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+    buffer.writeByte(2);
+    buffer.writeByte(0);
+
+    exception.expect(SWTProtocolException.class);
+    exception.expectMessage("Unexpected flags read from buffer. Expected 1, but read 0");
+
+    SWTInteger.readIntegerArray(buffer);
+  }
+
+  @Test
+  public void readIntegerArrayNegativeArrayLength() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+    buffer.writeByte(2);
+    buffer.writeByte(1);
+    buffer.writeShort(-1);
+
+    exception.expect(SWTProtocolException.class);
+    exception.expectMessage("Length of array cannot be negative");
+
+    SWTInteger.readIntegerArray(buffer);
+  }
+
+  @Test
+  public void readIntegerArrayEmpty() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+    buffer.writeByte(2);
+    buffer.writeByte(1);
+    buffer.writeShort(0);
+
+    assertThat(SWTInteger.readIntegerArray(buffer).length, is(0));
+  }
+
+  @Test
+  public void readIntegerArrayNonEmpty() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+    buffer.writeByte(2);
+    buffer.writeByte(1);
+    buffer.writeShort(2);
+    buffer.writeInt(1);
+    buffer.writeInt(2);
+
+    int result[] = SWTInteger.readIntegerArray(buffer);
+    assertThat(result.length, is(2));
+    assertThat(result[0], is(1));
+    assertThat(result[1], is(2));
+  }
+
+  @Test
   public void writeIntegerNullBuffer() {
     exception.expect(NullPointerException.class);
     exception.expectMessage("buffer cannot be null");
@@ -113,5 +182,47 @@ public class SWTIntegerTest {
     assertThat(buffer.readByte(), is((byte)2));
     assertThat(buffer.readByte(), is((byte)0));
     assertThat(buffer.readInt(), is(1));
+  }
+
+  @Test
+  public void writeIntegerArrayNullBuffer() throws SWTProtocolException {
+    exception.expect(NullPointerException.class);
+    exception.expectMessage("buffer cannot be null");
+
+    SWTInteger.writeIntegerArray(null, new int[] {});
+  }
+
+  @Test
+  public void writeIntegerArrayArrayTooLarge() throws SWTProtocolException {
+    int array[] = new int[Short.MAX_VALUE + 1];
+
+    exception.expect(SWTProtocolException.class);
+    exception.expectMessage("Length of array cannot be greater than 32767");
+
+    SWTInteger.writeIntegerArray(dynamicBuffer(), array);
+  }
+
+  @Test
+  public void writeIntegerArrayEmpty() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+
+    SWTInteger.writeIntegerArray(buffer, new int[] {});
+
+    assertThat(buffer.readByte(), is((byte)2));
+    assertThat(buffer.readByte(), is((byte)1));
+    assertThat(buffer.readShort(), is((short)0));
+  }
+
+  @Test
+  public void writeIntegerArrayNonEmpty() throws SWTProtocolException {
+    ChannelBuffer buffer = dynamicBuffer();
+
+    SWTInteger.writeIntegerArray(buffer, new int[] {1, 2});
+
+    assertThat(buffer.readByte(), is((byte)2));
+    assertThat(buffer.readByte(), is((byte)1));
+    assertThat(buffer.readShort(), is((short)2));
+    assertThat(buffer.readInt(), is(1));
+    assertThat(buffer.readInt(), is(2));
   }
 }
