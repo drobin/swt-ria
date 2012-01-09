73f70d3398a313e4eb12e171999135522b540b7c
Robin Doer <robin@robind.de>
2012-01-09 17:16:35 +0100

    Skeleton of FormData.attributeChanged()

--- de.robind.swt/src/org/eclipse/swt/layout/FormData.java
+++ de.robind.swt/src/org/eclipse/swt/layout/FormData.java
@@ -1,12 +1,17 @@
 package org.eclipse.swt.layout;
 
+import java.lang.reflect.Field;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.SWTObject;
 import org.eclipse.swt.server.ClientTasks;
 import org.eclipse.swt.server.DelayedCreation;
 import org.eclipse.swt.server.DisplayPool;
 import org.eclipse.swt.server.Key;
+import org.eclipse.swt.server.Trackable;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * Instances of this class are used to define the attachments of a control in
@@ -38,21 +43,25 @@ public class FormData extends SWTObject implements DelayedCreation {
   /**
    * left specifies the attachment of the left side of the control.
    */
+  @Trackable
   public FormAttachment left;
 
   /**
    * right specifies the attachment of the right side of the control.
    */
+  @Trackable
   public FormAttachment right;
 
   /**
    * top specifies the attachment of the top of the control.
    */
+  @Trackable
   public FormAttachment top;
 
   /**
    * bottom specifies the attachment of the bottom of the control.
    */
+  @Trackable
   public FormAttachment bottom;
 
   /**
@@ -60,6 +69,7 @@ public class FormData extends SWTObject implements DelayedCreation {
    * passed into Control.computeSize(int, int, boolean) to determine the
    * preferred size of the control. The default value is SWT.DEFAULT.
    */
+  @Trackable
   public int width = SWT.DEFAULT;
 
   /**
@@ -67,6 +77,7 @@ public class FormData extends SWTObject implements DelayedCreation {
    * passed into Control.computeSize(int, int, boolean) to determine the
    * preferred size of the control. The default value is SWT.DEFAULT.
    */
+  @Trackable
   public int height = SWT.DEFAULT;
 
   /**
@@ -99,5 +110,55 @@ public class FormData extends SWTObject implements DelayedCreation {
   public void createObject(Key key) throws Throwable {
     ClientTasks clientTasks = DisplayPool.getInstance().getClientTasks();
     clientTasks.createObject(key, getId(), getClass(), this.createArguments);
+  }
+
+  /**
+   * Invoked, if an attribute of the class has changed.
+   *
+   * @param field The name of the attribute-field, which has changed
+   */
+  public void attributeChanged(String field) {
+  }
+
+  /**
+   * Sends the update-attribute-request to the client.
+   *
+   * @param obj The object, which has changed
+   * @param field Name of the attribute, which has changed
+   * @throws SWTException if the request could not be sent
+   */
+  private static void changeAttribute(SWTObject obj, String field)
+      throws SWTException {
+
+    Object attrValue = getAttributeValue(obj, field);
+    Display display = Display.getCurrent();
+    display.updateAttribute(obj.getId(), field, attrValue);
+  }
+
+  /**
+   * Retrieves the value of the attribute with the given field-name.
+   *
+   * @param obj The object to read out
+   * @param field The name of the attribute-field
+   * @return The attribute-value
+   * @throws SWTException failed to retrieve the value
+   */
+  private static Object getAttributeValue(Object obj, String field)
+      throws SWTException {
+
+    try {
+      Field f = obj.getClass().getField(field);
+      return (f.get(obj));
+    } catch (NoSuchFieldException e) {
+      // TODO Here we need the correct code
+      SWTException exc = new SWTException(SWT.ERROR_UNSPECIFIED);
+      exc.throwable = e;
+      throw exc;
+    } catch (IllegalAccessException e) {
+      // TODO Here we need the correct code
+      SWTException exc = new SWTException(SWT.ERROR_UNSPECIFIED);
+      exc.throwable = e;
+      throw exc;
+    }
   }
 }
