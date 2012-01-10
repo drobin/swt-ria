9bbbe9a31f147b03fe5327bf31070dccafafcb4e
Robin Doer <robin@robind.de>
2012-01-10 12:40:46 +0100

    SWTObject.[get|set]Key()

--- de.robind.swt/src/org/eclipse/swt/SWTObject.java
+++ de.robind.swt/src/org/eclipse/swt/SWTObject.java
@@ -3,6 +3,8 @@ package org.eclipse.swt;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.swt.server.Key;
+
 
 
 /**
@@ -24,11 +26,45 @@ public class SWTObject {
   private static Map<Integer, SWTObject> objMap =
       new HashMap<Integer, SWTObject>();
 
+  /**
+   * The id of the object.
+   */
   private int id;
 
+  /**
+   * The key assigned to the object.
+   */
+  private Key key = null;
+
+  /**
+   * Creates a new {@link SWTObject}-instance and assigns an unique
+   * {@link #getId() id} to the instance.
+   */
   protected SWTObject() {
     this.id = generateId();
     SWTObject.objMap.put(this.id, this);
+  }
+
+  /**
+   * Creates a new {@link SWTObject}-instance and assigns a unique
+   * {@link #getId()} and the given <code>key</code> to the instance.
+   *
+   * @param key The key to assign
+   * @throws SWTException
+   *  <ul>
+   *    <li>{@link SWT#ERROR_NULL_ARGUMENT} -
+   *      if the key is <code>null</code>
+   *    </li>
+   *  </ul>
+   */
+  protected SWTObject(Key key) throws SWTException {
+    this();
+
+    if (key != null) {
+      setKey(key);
+    } else {
+      throw new SWTException(SWT.ERROR_NULL_ARGUMENT);
+    }
   }
 
   /**
@@ -49,6 +85,27 @@ public class SWTObject {
    */
   public static SWTObject findObjectById(int id) {
     return (SWTObject.objMap.get(id));
+  }
+
+  /**
+   * Returns the key, which is assigned (if any) to the instance.
+   *
+   * @return The assigned key
+   */
+  public Key getKey() {
+    return (this.key);
+  }
+
+  /**
+   * Assigns a key to the instance.
+   * <p>
+   * If <code>null</code> is passed to the method, the current assignment is
+   * removed.
+   *
+   * @param key The key to assign or <code>null</code> to remove assignment
+   */
+  protected void setKey(Key key) {
+    this.key = key;
   }
 
   /**
--- de.robind.swt/test/de/robind/swt/test/SWTObjectTest.java
+++ de.robind.swt/test/de/robind/swt/test/SWTObjectTest.java
@@ -1,5 +1,6 @@
 package de.robind.swt.test;
 
+import static de.robind.swt.test.utils.SWTExceptionMatcher.swtCode;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsSame.sameInstance;
@@ -8,11 +9,18 @@ import static org.junit.Assert.assertThat;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTObject;
+import org.eclipse.swt.server.Key;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class SWTObjectTest {
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+
   @SuppressWarnings("unchecked")
   @Before
   public void before() throws Exception {
@@ -49,5 +57,43 @@ public class SWTObjectTest {
     assertThat(SWTObject.findObjectById(2), is(sameInstance(obj2)));
     assertThat(SWTObject.findObjectById(3), is(sameInstance(obj3)));
     assertThat(SWTObject.findObjectById(4), is(nullValue()));
+  }
+
+  @Test
+  public void keyCtorNullArgument() {
+    exception.expect(swtCode(SWT.ERROR_NULL_ARGUMENT));
+    new SWTObject(null) {};
+  }
+
+  @Test
+  public void getKey() {
+    Key key = new Key() {};
+    SWTObject obj = new SWTObject(key) {};
+    assertThat(obj.getKey(), is(sameInstance(key)));
+  }
+
+  @Test
+  public void setKey() {
+    Key key1 = new Key() {};
+    Key key2 = new Key() {};
+    class TestSWTObject extends SWTObject {
+      public TestSWTObject(Key key) {
+        super(key);
+      }
+
+      public void setKeyTest(Key key) {
+        setKey(key);
+      }
+    };
+
+    TestSWTObject obj = new TestSWTObject(key1);
+
+    assertThat(obj.getKey(), is(sameInstance(key1)));
+
+    obj.setKeyTest(null);
+    assertThat(obj.getKey(), is(nullValue()));
+
+    obj.setKeyTest(key2);
+    assertThat(obj.getKey(), is(sameInstance(key2)));
   }
 }
