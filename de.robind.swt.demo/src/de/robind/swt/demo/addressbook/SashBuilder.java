4c47ded26ee5aa36e457dba55c489fb3af0af913
Robin Doer <robin@robind.de>
2012-01-11 16:02:28 +0100

    Improve performance of the Sash

--- de.robind.swt.demo/src/de/robind/swt/demo/addressbook/SashBuilder.java
+++ de.robind.swt.demo/src/de/robind/swt/demo/addressbook/SashBuilder.java
@@ -1,55 +1,56 @@
 package de.robind.swt.demo.addressbook;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Sash;
 
 public class SashBuilder {
-  public static void buildSash(final Composite parent, Control left, Control right) {
+  public static void buildSash(Composite parent, Control left, Control right) {
+    parent.setLayout(new FormLayout());
+
+    // Create the sash first, so the other controls
+    // can be attached to it.
     final Sash sash = new Sash(parent, SWT.VERTICAL);
+    FormData data = new FormData();
+    data.top = new FormAttachment(0, 0); // Attach to top
+    data.bottom = new FormAttachment(100, 0); // Attach to bottom
+    data.left = new FormAttachment(50, 0); // Attach halfway across
+    sash.setLayoutData(data);
 
-    FormData leftData = new FormData();
-    leftData.left = new FormAttachment(0, 0);
-    leftData.right = new FormAttachment(sash, 0);
-    leftData.top = new FormAttachment(0, 0);
-    leftData.bottom = new FormAttachment(100, 0);
-    left.setLayoutData(leftData);
+    sash.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent event) {
+        // We reattach to the left edge, and we use the x value of the event to
+        // determine the offset from the left
+        ((FormData)sash.getLayoutData()).left = new FormAttachment(0, event.x);
 
-    FormData rightData = new FormData();
-    rightData.left = new FormAttachment(sash, 0);
-    rightData.right = new FormAttachment(100, 0);
-    rightData.top = new FormAttachment(0, 0);
-    rightData.bottom = new FormAttachment(100, 0);
-    right.setLayoutData(rightData);
-
-    final int limit = 20, percent = 50;
-    final FormData sashData = new FormData();
-    sashData.left = new FormAttachment(percent, 0);
-    sashData.top = new FormAttachment(0, 0);
-    sashData.bottom = new FormAttachment(100, 0);
-    sash.setLayoutData(sashData);
-    sash.addListener(SWT.Selection, new Listener() {
-      public void handleEvent(Event e) {
-        Rectangle sashRect = sash.getBounds();
-        Rectangle shellRect = parent.getClientArea();
-        int right = shellRect.width - sashRect.width - limit;
-        e.x = Math.max(Math.min (e.x, right), limit);
-        if (e.x != sashRect.x) {
-          sashData.left = new FormAttachment(0, e.x);
-          parent.layout();
-        }
+        // Until the parent window does a layout, the sash will not be redrawn in
+        // its new location.
+        sash.getParent().layout();
       }
     });
 
-    FormLayout form = new FormLayout();
-    parent.setLayout(form);
+    // Create the first text box and attach its right edge
+    // to the sash
+    data = new FormData();
+    data.top = new FormAttachment(0, 0);
+    data.bottom = new FormAttachment(100, 0);
+    data.left = new FormAttachment(0, 0);
+    data.right = new FormAttachment(sash, 0);
+    left.setLayoutData(data);
 
+    // Create the second text box and attach its left edge
+    // to the sash
+    data = new FormData();
+    data.top = new FormAttachment(0, 0);
+    data.bottom = new FormAttachment(100, 0);
+    data.left = new FormAttachment(sash, 0);
+    data.right = new FormAttachment(100, 0);
+    right.setLayoutData(data);
   }
 }
