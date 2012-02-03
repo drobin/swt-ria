9bf6a5eac02a24a3298724ff209d0587dcc77a29
Robin Doer <robin@robind.de>
2012-02-03 17:35:12 +0100

    Key

---
+++ de.robind.swt.base/src/main/java/de/robind/swt/base/Key.java
@@ -0,0 +1,12 @@
+package de.robind.swt.base;
+
+/**
+ * The key identifies a running application for an external application-server.
+ * <p>
+ * The application-server needs to provide a useful implementation.
+ *
+ * @author Robin Doer
+ */
+public interface Key {
+
+}
